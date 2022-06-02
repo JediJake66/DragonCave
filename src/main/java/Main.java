2c03ee63b0fac3677ceda79b15a0ca197ca8a804
JediJake66 <jacobstout59@gmail.com>
2022-06-02 15:19:49 -0400

    added try catch

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Dragon Land!!");
+        System.out.println("You start exploring the new land and come across two caves....");
+        System.out.println("In one cave you will find a friendly dragon that is very generous and wants to share his treasure!");
+        System.out.println("But in the other, there's a dragon just dying to cook you up and eat you....");
+        System.out.println("Which cave will you choose?");
+        System.out.println("Type 1 or 2");
+        Scanner getInput = new Scanner(System.in);
+        int input=0;
+        try {
+            input = getInput.nextInt();
+        }catch(Exception e){
+            System.out.println("Caught Exception: Input not a number");
+        }
+        if(input==1){
+            System.out.println("You enter the cave and come upon a dragon relaxing back on a pile of gold");
+            System.out.println("He then says to you 'Hello my friend! Care for some gold?'");
+            System.out.println("You graciously accept and leave the cave unscathed with much more riches than before");
+        }else{
+            System.out.println("As you enter the cave you notice things are starting to get warmer");
+            System.out.println("You then round a corner into the treasure hold.....");
+            System.out.println("....and get burnt to a crisp");
+            System.out.println("YOU'RE DINNER!!!");
+        }
+    }
+}
+
